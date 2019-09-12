5af929e6f5a8b2c0ebc48369709e84d49970a8b2
mFiedler <m.fiedler@mvolution.de>
2019-09-12 16:05:58 +0200

    tests being optimized, new JSON2CalDAVParser class implemented

--- src/main/java/com/mvolution/radicale/BaseCaldavClient.java
+++ src/main/java/com/mvolution/radicale/BaseCaldavClient.java
@@ -26,29 +26,44 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-
+// a class to communicate with the CalDAV-Server and send requests using REST-API
 public class BaseCaldavClient {
+    // instance variable that represents the default uri to calendar-collecction
     private static final String DEFAULT_URI = "http://localhost:5232/mvolution/91baf471-dbd6-9299-b238-0465e36b8f5e/";
+    // object for logging the events of this class
     private final Logger LOG = LoggerFactory.getLogger(BaseCaldavClient.class);
+    // instance variable sets the desired uri (default uri by standard)
     private String uri = DEFAULT_URI;
 
+    // empty constructor
     public BaseCaldavClient() {    }
+    /* constructor
+    * @param uri - the uri to the desired calendar collection*/
+    public BaseCaldavClient(String uri) {
+        this.uri = uri;
+    }
 
+    // method that prints all entries from the given calendar-path
     @Test
-    public void printAllEntries() throws IOException {
+    public void printAllEntries() {
+        // instantiate a HTTP-Client Object with default credentials
         CloseableHttpClient httpClient = HttpClients.createDefault();
         try  {
+            // instantiate a GenerateQuery and passing a generic query to a new CalendarQuery by using generate()
             GenerateQuery gq = new GenerateQuery();
             CalendarQuery calQ = gq.generate();
 
+            // instantiate a new CalDAVCollection with the path to the collection
             CalDAVCollection collection = new CalDAVCollection(this.uri);
 
+            // instantiate a List of Calendars with the previously set of credentials
             List<Calendar> calendars = collection.queryCalendars(httpClient, calQ);
+            // counter for printing the position of the event
             int eventCounter = 1;
+            // iterating through the calendars and events and print them
             for (Calendar calendar : calendars) {
                 ComponentList componentList = calendar.getComponents().getComponents(Component.VEVENT);
                 for (VEvent ve : (Iterable<VEvent>) componentList) {
-
                     LOG.info("\nEvent-Nr.: --->[ " + eventCounter++ + " ]<--- \n" + ve.toString());
                 }
             }
@@ -57,19 +72,23 @@ public class BaseCaldavClient {
         }
     }
 
+    /* a method to create and save an all day-event to our calendar
+    * @param date - the desired date for the event
+    * @param summary - the summary of our event*/
     @Test
-    public void createAllDayEvent() {
+    public void createAllDayEvent(String date, String summary) {
+        // instantiate a HTTP-Client Object with default credentials
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         try {
+            // instantiate a new CalDAVCollection with the path to the collection
              CalDAVCollection collection = new CalDAVCollection(this.uri);
-
-             Date start = new Date("20190920");
-             VEvent testEvent = new VEvent(start, "the first testing of an created event");
-
+             // instantiate a new Date Object with the parameter date
+             Date start = new Date(date);
+             // instantiate a new VEvent Object with Date-Object and the summary as parameter
+             VEvent testEvent = new VEvent(start, summary);
+             // calling the add() and passing the httpClient & event as parameter to
              collection.add(httpClient, testEvent, null);
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/mvolution/radicale/Main.java
+++ src/main/java/com/mvolution/radicale/Main.java
@@ -3,14 +3,25 @@ package com.mvolution.radicale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class Main{
-
+    // object for logging the events of this class
     static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
+
     public static void main(String args[]) {
+        // creating an instance of our BaseCalDAVClient
+        BaseCaldavClient client = new BaseCaldavClient();
+        // calling the method that prints all calendar entries
+        client.printAllEntries();
+        // calling the method for create and save an all day event
+        client.createAllDayEvent("20191005", "Franks Geburtstagsfeier");
 
-
+        // creating an instance of our JSON2CaldavParser with setting the path to json-file
+        JSON2CalDAVParser flight = new JSON2CalDAVParser("C:\\Users\\mvolution\\IdeaProjects\\radicaleCalDAV_test\\res\\metaplanner-flight.json");
+        // calling the method that print the JSON-content a String
+        flight.printJSONFile();
+        // printing the corresponding value to the given key with help of the logging object
+        LOG.info("----------------> JSONObject Response: " + flight.getJSONContent("@calendarId"));
 
 
 
